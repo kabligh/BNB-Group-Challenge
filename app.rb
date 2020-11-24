116c192e11b70787373fcd8be69fbbd6e927100a
Francesco <francescofacca@gmail.com>
2020-11-24 15:29:57 +0000

    small changes on app.rb

--- app.rb
+++ app.rb
@@ -11,7 +11,11 @@ class BnB < Sinatra::Base
 
   post '/users' do
     User.create(name: params[:name], email: params[:email], password: params[:password])
-    redirect '/'
+    redirect '/spaces'
+  end
+
+  get '/spaces' do
+    'Choose a b&b!'
   end
 
   run! if app_file == $0
