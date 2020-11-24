a22ad538685b34e741bc778077783d4532d97df2
Margarida Martelo <margmartelo@gmail.com>
2020-11-24 16:38:22 +0000

    Added feature of encrypted password and readjustment of password's lenght in order to enable it'

--- lib/user.rb
+++ lib/user.rb
@@ -1,4 +1,5 @@
 require_relative './database_connection'
+require 'bcrypt'
 
 class User
   attr_reader :id, :name
@@ -9,9 +10,10 @@ class User
   end
 
   def self.create(name:, email:, password:)
+    encrypted_password = BCrypt::Password.create(password)
     entry = DatabaseConnection.query(
       "INSERT INTO users (name, email, password)
-       VALUES('#{name}', '#{email}', '#{password}')
+       VALUES('#{name}', '#{email}', '#{encrypted_password}')
        RETURNING id, name;")
     User.new(id: entry[0]['id'], name: entry[0]['name'])
   end
