f072d7970d1788416074c335a4e74d3c0ee5fbe9
Michael Schats <michael_schats@hotmail.com>
2020-11-25 16:48:19 +0000

    Deleted all from and to dates

--- spec/features/viewing_spaces_spec.rb
+++ spec/features/viewing_spaces_spec.rb
@@ -4,10 +4,10 @@ feature 'View spaces' do
   scenario 'User can see all spaces for hire' do
     Space.create(name: 'Cosy cabin', user_id: '1',
       description: 'Escape to the countryside and relax by a log fire',
-      price: 100, from: '2020-12-19', to: '2021-01-03')
+      price: 100)
     Space.create(name: 'London loft', user_id: '2',
       description: 'Live la vida loca',
-      price: 200, from: '2021-01-29', to: '2021-02-03')
+      price: 200)
 
     visit '/spaces'
     expect(page).to have_content('Cosy cabin')
--- spec/unit/spaces_spec.rb
+++ spec/unit/spaces_spec.rb
@@ -6,14 +6,12 @@ describe Space do
     it 'creates a new space' do
       space = Space.create(name: 'Cosy cabin', user_id: '1',
         description: 'Escape to the countryside and relax by a log fire',
-        price: '100', from: '2020-12-19', to: '2021-01-03')
+        price: '100')
 
       expect(space.name).to eq 'Cosy cabin'
       expect(space.user_id).to eq '1'
       expect(space.description).to eq 'Escape to the countryside and relax by a log fire'
       expect(space.price).to eq '100'
-      expect(space.from).to eq '2020-12-19'
-      expect(space.to).to eq '2021-01-03'
     end
   end
 
@@ -21,10 +19,10 @@ describe Space do
     it 'lists all the spaces' do
       space = Space.create(name: 'Cosy cabin', user_id: '1',
         description: 'Escape to the countryside and relax by a log fire',
-        price: 100, from: '2020-12-19', to: '2021-01-03')
+        price: 100)
       Space.create(name: 'London loft', user_id: '2',
         description: 'Live la vida loca',
-        price: 200, from: '2021-01-29', to: '2021-02-03')
+        price: 200)
 
       spaces = Space.all
 
@@ -35,5 +33,4 @@ describe Space do
       expect(spaces.first.id).to eq space.id
     end
   end
-
 end
