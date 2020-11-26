22431e09e07d6dfc91e8c2948cd79aa8e9203634
K Annabel Bligh <kabligh@gmail.com>
2020-11-26 17:49:52 +0000

    Fixed dates appearing

--- lib/spaces.rb
+++ lib/spaces.rb
@@ -2,29 +2,34 @@ require 'pg'
 
 class Space
 
-  attr_reader :id, :name, :user_id, :description, :price, :from, :to
+  attr_reader :id, :name, :user_id, :description, :price, :from_date, :to_date
 
-  def initialize(id:, name:, user_id:, description:, price:, from:, to:)
+  def initialize(id:, name:, user_id:, description:, price:, from_date:, to_date:)
     @id = id
     @name = name
     @user_id = user_id
     @description = description
     @price = price
-    @from = from
-    @to = to
+    @from_date = from_date
+    @to_date = to_date
   end
 
-  def self.create(name:, user_id:, description:, price:, from:, to:)
+  def self.create(name:, user_id:, description:, price:, from_date:, to_date:)
     result = DatabaseConnection.query(
       "INSERT INTO spaces(name, user_id, description, price, from_date, to_date)
-      VALUES('#{name}', '#{user_id}', '#{description}', #{price}, '#{from}', '#{to}')
+      VALUES('#{name}', '#{user_id}', '#{description}', #{price}, '#{from_date}', '#{to_date}')
       RETURNING id, user_id, name, description, price, from_date, to_date;")
-    Space.new(id: result[0]["id"], name: result[0]["name"], user_id: result[0]["user_id"], description: result[0]["description"], price: result[0]["price"], from: result[0]["from_date"], to: result[0]["to_date"])
+    Space.new(id: result[0]["id"], name: result[0]["name"], user_id: result[0]["user_id"], description: result[0]["description"], price: result[0]["price"], from_date: result[0]["from_date"], to_date: result[0]["to_date"])
   end
 
   def self.all
     result = DatabaseConnection.query('SELECT * FROM spaces;')
-    result.map { |space| Space.new(id: space['id'], name: space['name'], user_id: space['user_id'], description: space['description'], price: space['price'], from: space['from'], to: space['to']) }
+    result.map { |space| Space.new(id: space['id'], name: space['name'], user_id: space['user_id'], description: space['description'], price: space['price'], from_date: space['from_date'], to_date: space['to_date']) }
+  end
+
+  def self.available(from_date:, to_date:)
+    result = DatabaseConnection.query("SELECT * FROM spaces WHERE from_date >= '#{from_date}' AND to_date <= '#{to_date}';")
+    result.map { |space| Space.new(id: space['id'], name: space['name'], user_id: space['user_id'], description: space['description'], price: space['price'], from_date: space['from_date'], to_date: space['to_date']) }
   end
 
 end
--- spec/unit/spaces_spec.rb
+++ spec/unit/spaces_spec.rb
@@ -6,14 +6,14 @@ describe Space do
     it 'creates a new space' do
       space = Space.create(name: 'Cosy cabin', user_id: '1',
         description: 'Escape to the countryside and relax by a log fire',
-        price: '100', from: '2020-12-19', to: '2021-01-03')
+        price: '100', from_date: '2020-12-19', to_date: '2021-01-03')
 
       expect(space.name).to eq 'Cosy cabin'
       expect(space.user_id).to eq '1'
       expect(space.description).to eq 'Escape to the countryside and relax by a log fire'
       expect(space.price).to eq '100'
-      expect(space.from).to eq '2020-12-19'
-      expect(space.to).to eq '2021-01-03'
+      expect(space.from_date).to eq '2020-12-19'
+      expect(space.to_date).to eq '2021-01-03'
     end
   end
 
@@ -21,10 +21,10 @@ describe Space do
     it 'lists all the spaces' do
       space = Space.create(name: 'Cosy cabin', user_id: '1',
         description: 'Escape to the countryside and relax by a log fire',
-        price: 100, from: '2020-12-19', to: '2021-01-03')
+        price: 100, from_date: '2020-12-19', to_date: '2021-01-03')
       Space.create(name: 'London loft', user_id: '2',
         description: 'Live la vida loca',
-        price: 200, from: '2021-01-29', to: '2021-02-03')
+        price: 200, from_date: '2021-01-29', to_date: '2021-02-03')
 
       spaces = Space.all
 
@@ -36,4 +36,25 @@ describe Space do
     end
   end
 
+  describe '.available' do
+    it 'gives available spaces' do
+      house = Space.create(name: 'Some house', user_id: '5',
+        description: 'Some house somewhere',
+        price: 100, from_date: '2020-12-25', to_date: '2020-12-31')
+      p house
+
+      house1 = Space.create(name: 'Some house1', user_id: '6',
+        description: 'Some house somewhere',
+        price: 100, from_date: '2020-12-20', to_date: '2020-12-29')
+
+      house2 = Space.create(name: 'Some house2', user_id: '7',
+        description: 'Some house somewhere',
+        price: 100, from_date: '2020-12-18', to_date: '2020-12-22')
+
+      request = Space.available(from_date: '2020-12-25', to_date: '2020-12-31')
+      p request
+
+      expect(request.first.name).to eq "Some house"
+    end
+  end
 end
